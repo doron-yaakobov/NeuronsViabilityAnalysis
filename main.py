4dd9a410de03f83d53bec1d5881e42e4f1864cce
dorony <Dy209495381>
2022-12-20 11:02:59 +0200

    MVP

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import csv
 from datetime import datetime
-
 import pandas as pd
+from statsmodels.stats.multitest import fdrcorrection as fdr
+from IPython.display import display
 from attrdict import AttrDict
 import matplotlib.pyplot as plt
+import numpy as np
+import pingouin as pg
+from scipy import stats
 
 SRC = "Ubiquitome_AchillesData.xlsx"
 NERVOUS_SUFFIX = "CENTRAL_NERVOUS_SYSTEM"
+HOMOGENITY_THRESHOLD = 0.5
+i = 0
+results = list()
 
 if __name__ == '__main__':
     src = pd.read_excel(SRC)
     src = src.reset_index()
     koGenes = AttrDict()
     avgDeltas = AttrDict()
-    subjects = ['Total', 'Nervous', 'Not Nervous', 'Delta', "Gene"]
-    results = [subjects]
     for _, koGeneData in src.iterrows():
         koGeneKey = koGeneData["Class"] + "-" + koGeneData["Gene"]
         koGeneMeanViability = koGeneData["Mean"]
@@ -26,69 +31,109 @@ if __name__ == '__main__':
 
         koGenes[koGeneKey] = AttrDict({
             "mean": koGeneMeanViability,
+            "TtestPValue": None,
+            "TT_Type": None,
             "nervous": AttrDict({
+                "values": np.zeros(0),
                 "accumulated_val": 0,
-                "amount": 0,
                 "avg": None
             }),
             "notNervous": AttrDict({
+                "values": np.zeros(0),
                 "accumulated_val": 0,
-                "amount": 0,
                 "avg": None
             })
         })
 
         cellLines = koGeneData.keys()
         for cellLine in cellLines:
-            is_nervous = NERVOUS_SUFFIX in cellLine.split("_", 1)
-            koGenes[koGeneKey]["nervous" if is_nervous else "notNervous"]["accumulated_val"] += koGeneData[cellLine]
-            koGenes[koGeneKey]["nervous" if is_nervous else "notNervous"]["amount"] += 1
-            koGenes[koGeneKey]["nervous" if is_nervous else "notNervous"]["avg"] = \
-                koGenes[koGeneKey]["nervous" if is_nervous else "notNervous"]["accumulated_val"] / \
-                koGenes[koGeneKey]["nervous" if is_nervous else "notNervous"]["amount"]
-
+            is_nervous = "nervous" if NERVOUS_SUFFIX in cellLine.split("_", 1) else "notNervous"
+            curr_cell_department = koGenes[koGeneKey][is_nervous]
+            curr_cell_department["accumulated_val"] += koGeneData[cellLine]
+            curr_cell_department["values"] = np.append(curr_cell_department["values"], koGeneData[cellLine])
+        koGenes[koGeneKey]["nervous"]["avg"] = koGenes[koGeneKey]["nervous"]["accumulated_val"] / \
+                                               koGenes[koGeneKey]["nervous"]["values"].size
+        koGenes[koGeneKey]["notNervous"]["avg"] = koGenes[koGeneKey]["notNervous"]["accumulated_val"] / \
+                                                  koGenes[koGeneKey]["notNervous"]["values"].size
         avgDeltas[koGeneKey] = koGenes[koGeneKey]["nervous"]["avg"] - koGenes[koGeneKey]["notNervous"]["avg"]
-        # plot
 
+        # Assuming Normality assumption (Empiric results)
+        # Test Homogeneity assumption:
+        is_homogenic = stats.levene(koGenes[koGeneKey]["nervous"]["values"],
+                                    koGenes[koGeneKey]["notNervous"]["values"]).pvalue > HOMOGENITY_THRESHOLD
+        # Calculate t-test:
+        koGenes[koGeneKey]["TT_Type"] = "Student's t-test" if is_homogenic else "Welch's t-test"
+        tTestRes = pg.ttest(koGenes[koGeneKey]["nervous"]["values"], koGenes[koGeneKey]["notNervous"]["values"],
+                            correction=not is_homogenic)
+        koGenes[koGeneKey]["TtestPValue"] = tTestRes["p-val"][0]
+
+        # display(res)
+
+        # ignore irrelevant data:
+        if not (koGenes[koGeneKey]["TtestPValue"] < 0.05):
+            continue
+        # if fdr(np.array([tTestRes["p-val"][0]]), alpha=0.1)[0][0]:
+        # print(f"{fdr(np.array([res['p-val'][0]]), alpha=10)[0][0]}")
+        # pass
+        # print("hi1")
+        # continue
+        i += 1
+        print(i)
+
+        # plot
         scores = [koGenes[koGeneKey]["mean"], koGenes[koGeneKey]["nervous"]["avg"],
                   koGenes[koGeneKey]["notNervous"]["avg"], avgDeltas[koGeneKey]]
-        # fig, ax = plt.subplots(figsize=(7, 5))
-        # ax.bar(subjects, scores)
-        # ax.xaxis.set_tick_params(pad=10)
-        # ax.yaxis.set_tick_params(pad=5)
-        # ax.grid(b=True, color='gray', linestyle='-.', linewidth=0.75, alpha=0.2)
-        #
-        # # Add Plot Title
-        # ax.set_title('Viability Means - {} KO'.format(koGeneKey))
-        #
-        # # Add Text watermark
-        # fig.text(0.9, 0.15, "Koren's lab", fontsize=12,
-        #          color='gray', ha='right', va='bottom',
-        #          alpha=0.7)
-        #
-        # plt.savefig('Plots/{} KO.png'.format(koGeneKey), dpi=200, format='png', bbox_inches='tight')
-        # plt.close()
-        scores.append(koGeneKey)
-        results.append(scores)
+        subjects = ['Total', 'Nervous', 'Not Nervous', 'Delta']  # Gene
+        fig, ax = plt.subplots(figsize=(7, 5))
+        ax.bar(subjects, scores)
+        ax.xaxis.set_tick_params(pad=10)
+        ax.yaxis.set_tick_params(pad=5)
+        ax.grid(b=True, color='gray', linestyle='-.', linewidth=0.75, alpha=0.2)
 
-    # log results
-    date = datetime.now().strftime("%Y.%m.%d_%H-%M-%S")
-    file_name = "{} Neurons Viability Score Results.csv".format(date)
+        # Add Plot Title
+        ax.set_title(f'Viability Means - {koGeneKey} KO')
 
-    with open(file_name, 'w') as results_csv:
-        writer = csv.writer(results_csv)
-        writer.writerows(results)
+        # Add Text watermark
+        fig.text(0.9, 0.15, f"{koGenes[koGeneKey]['TT_Type']} validated", fontsize=12, color='gray', ha='right',
+                 va='bottom', alpha=0.7)
 
-    csv_data = pd.read_csv(file_name)
-    csv_data.to_excel(file_name.replace("csv", "xlsx"), index=None, header=True)
+        plt.savefig(f'Plots/{koGenes[koGeneKey]["TT_Type"]}/{koGeneKey} KO.png', dpi=200, format='png',
+                    bbox_inches='tight')
+        plt.close()
+        # scores.append(koGeneKey)
+        results.append(
+            {
+                "Total": koGenes[koGeneKey]["mean"],
+                "Nervous": koGenes[koGeneKey]["nervous"]["avg"],
+                "Not Nervous": koGenes[koGeneKey]["notNervous"]["avg"],
+                "Delta": avgDeltas[koGeneKey],
+                "Gene": koGeneKey,
+                "T Test Type": koGenes[koGeneKey]["TT_Type"],
+                "T Test P Value": koGenes[koGeneKey]["TtestPValue"]
+            }
+        )
+# log results
+subjects.append("Gene", )
+subjects.append("T Test Type")
+subjects.append("T Test P Value")
+date = datetime.now().strftime("%Y.%m.%d_%H-%M-%S")
+file_name = f"{date} Neurons Viability Score Results.csv"
 
-    sorted_deltas = sorted(avgDeltas, key=avgDeltas.get, reverse=True)
+with open(file_name, 'w') as results_csv:
+    writer = csv.DictWriter(results_csv, subjects)
+    writer.writeheader()
+    writer.writerows(results)
 
-    file_name = "{} Averages Sorted Deltas (Big to Small).csv".format(date)
+csv_data = pd.read_csv(file_name)
+csv_data.to_excel(file_name.replace("csv", "xlsx"), index=None, header=True)
 
-    with open(file_name, 'w') as results_csv:
-        writer = csv.writer(results_csv)
-        writer.writerows(zip(avgDeltas.keys(), avgDeltas.values()))
+# sorted_deltas = sorted(avgDeltas, key=avgDeltas.get, reverse=True)
 
-    csv_data = pd.read_csv(file_name)
-    csv_data.to_excel(file_name.replace("csv", "xlsx"), index=None, header=True)
+# file_name = "{} Averages Sorted Deltas (Big to Small).csv".format(date)
+#
+# with open(file_name, 'w') as results_csv:
+#     writer = csv.writer(results_csv)
+#     writer.writerows(zip(avgDeltas.keys(), avgDeltas.values()))
+#
+# csv_data = pd.read_csv(file_name)
+# csv_data.to_excel(file_name.replace("csv", "xlsx"), index=None, header=True)
